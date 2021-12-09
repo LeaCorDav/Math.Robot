ff0da6d86b301cf3f2522c49199c76419c8002fb
Leandro <leaa_67@hotmail.com>
2021-12-09 12:41:42 -0600

    Diferentes triangulos

--- figuras.js
+++ figuras.js
@@ -13,9 +13,18 @@ function perimetroTriangulo(arista1, arista2, base){
     return parseInt(arista1) + parseInt(arista2) + parseInt(base);
 }
 
-function areaTriangulo(arista1, base){
+function areaTrianguloEquiIso(arista1, base){
     const alturaTriangulo = (Math.sqrt((arista1 ** 2) - ((base / 2)** 2)));
     return (base * alturaTriangulo) / 2;
+}
+
+function areaTrianguloRec(arista2, base){
+    return (base * arista2) / 2;
+}
+
+function areaTrianguloEtc(arista1, arista2, base){
+    const semiper = perimetroTriangulo(arista1, arista2, base) / 2;       
+    return Math.sqrt(semiper * (semiper - arista1) * (semiper - arista2) * (semiper - base));
 }
 
 
@@ -64,12 +73,25 @@ function calcularPerimetroTriangulo(){
 
 function calcularAreaTriangulo(){
     const input1 = document.getElementById("InputTriangulo1");
+    const input2 = document.getElementById("InputTriangulo2");
     const input3 = document.getElementById("InputTriangulo3");
     const valor1 = input1.value;
+    const valor2 = input2.value;
     const valorbase = input3.value;
+    const check = document.querySelector('input[name="check"]:checked').value;    
 
-    const area = areaTriangulo(valor1, valorbase);
-    alert(area);
+    if (check == "eqis"){
+        const area = areaTrianguloEquiIso(valor1, valorbase);
+        alert(area);
+    } else if (check == "rect"){
+        const area = areaTrianguloRec(valor2, valorbase);
+        alert(area);
+    } else if (check == "otro"){
+        const area = areaTrianguloEtc(valor1, valor2, valorbase);
+        alert(area);
+    } else {
+        alert("Choose the triangle's type");
+    }
 }
 
 function calcularPerimetroCirculo(){
