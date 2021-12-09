1c5aea96281a2206ca3f2441c4a183d699d88526
Leandro <leaa_67@hotmail.com>
2021-12-08 22:55:21 -0600

    JS con funciones y parametros

--- figuras.js
+++ figuras.js
@@ -1,39 +1,31 @@
 // Cuadrado
-let ladoCuadrado = 5;
+function perimetroCuadrado (arista){
+    return arista * 4;
+}
 
-const perimetroCuadrado = ladoCuadrado * 4;
-const areaCuadrado = ladoCuadrado ** 2;
+function areaCuadrado(arista){
+    return arista ** 2;
+}
 
-console.group("Cuadrado");
-console.log ("Los lados del cuadrado miden: " + ladoCuadrado + " pixeles")
-console.log ("El perimetro del cuadrado mide: " + perimetroCuadrado + " pixeles");
-console.log ("El área del cuadrado mide: " + areaCuadrado + " pixeles^2");
-console.groupEnd();
 
 // Triangulo
-let ladoTriangulo1 = 10;
-let ladoTriangulo2 = 4;
-let baseTriangulo = 16;
-const alturaTriangulo = (Math.sqrt((ladoTriangulo1 ** 2) - ((baseTriangulo / 2)** 2)));
+function perimetroTriangulo(arista1, arista2, base){
+    return arista1 + arista2 + base;
+}
 
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+function areaTriangulo(arista1, base){
+    const alturaTriangulo = (Math.sqrt((arista1 ** 2) - ((base / 2)** 2)));
+    return (base * alturaTriangulo) / 2;
+}
 
-console.group("Triangulo");
-console.log ("Los lados del triangulo miden: " + ladoTriangulo1 + ", " + ladoTriangulo2 + ", " + baseTriangulo + " pixeles");
-console.log ("El perimetro del triangulo mide: " + perimetroTriangulo + " pixeles");
-console.log ("El área del triangulo mide: " + perimetroTriangulo + " pixeles^2");
-console.groupEnd();
 
 //Circulo
-let radioCirculo = 4;
-let diametroCirculo = radioCirculo * 2;
+const diametroCirculo = (radio) => radio * 2;
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * Math.PI;
+}
 
-const perimetroCirculo = diametroCirculo * Math.PI;
-const areaCirculo = (radioCirculo ** 2) * Math.PI;
-
-console.group("Circulo");
-console.log ("El radio del circulo mide: " + radioCirculo + " pixeles y un diametro de " + diametroCirculo + " pixeles");
-console.log ("El perimetro del circulo mide: " + perimetroCirculo + " pixeles");
-console.log ("El área del circulo mide: " + areaCirculo + " pixeles^2");
-console.groupEnd();
+function areaCirculo(radio){
+    return (radio ** 2) * Math.PI;
+}
