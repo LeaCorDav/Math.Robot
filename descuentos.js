42e1e072f946651ae381fd90d68a170630b3913a
Leandro <leaa_67@hotmail.com>
2021-12-10 16:22:22 -0600

    Ya funciona basicamente la pagina

--- descuentos.js
+++ descuentos.js
@@ -1,9 +1,17 @@
-
-
-
 function calcularPrecioconDescuento(precio, descuento){
     const porcentajePrecio = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecio) / 100;
 
     return precioConDescuento;
+}
+
+
+function precioDescontado(){
+    const inputPrecio = document.getElementById("InputPrecio").value;
+    const inputDescuento = document.getElementById("InputDescuento").value;
+
+    const precioFinal = calcularPrecioconDescuento(inputPrecio, inputDescuento);
+
+    const resultado = document.getElementById("Resultado");
+    resultado.innerText = "The final ammount you would have to pay is: $" + precioFinal;
 }
