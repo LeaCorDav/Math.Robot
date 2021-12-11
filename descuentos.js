89d2267c701947b3d195afa3331ad5a8b75c6c14
Leandro <leaa_67@hotmail.com>
2021-12-10 22:11:58 -0600

    Pagina de descuentos hecho, falta CSS

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,4 @@
+// Formula principal
 function calcularPrecioconDescuento(precio, descuento){
     const porcentajePrecio = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecio) / 100;
@@ -5,7 +6,8 @@ function calcularPrecioconDescuento(precio, descuento){
     return precioConDescuento;
 }
 
-
+//________________________________________________________________
+// Introduce el precio y el descuento para obtener un precio final
 function precioDescontado(){
     const inputPrecio = document.getElementById("InputPrecio").value;
     const inputDescuento = document.getElementById("InputDescuento").value;
@@ -14,4 +16,49 @@ function precioDescontado(){
 
     const resultado = document.getElementById("Resultado");
     resultado.innerText = "The final ammount you would have to pay is: $" + precioFinal;
+}
+
+//________________________________________________________________
+// Para cupones
+const cupones = [
+    {
+        nombre: "SPRING_DSC",
+        descuento: 15,
+    },
+    {
+        nombre: "SUMMER_DSC",
+        descuento: 10,
+    },
+    {
+        nombre: "INDEPENDENCEDAY_DSC",
+        descuento: 30,
+    },
+    {
+        nombre: "BLACKFRIDAY_DSC",
+        descuento: 75,
+    },
+    {
+        nombre: "XMASS_DSC",
+        descuento: 25,
+    },    
+];
+
+function precioDescontadoCupon(){
+    const inputPrecio = document.getElementById("InputPrecio2").value;
+    const inputCupon = document.getElementById("InputCupon").value;    
+    
+    const validandoCupon = function(cupon){
+        return cupon.nombre === inputCupon;
+    }
+    
+    const usuarioCupon = cupones.find(validandoCupon);
+    
+    if (!usuarioCupon){
+        alert ("The coupon " + inputCupon + " is not valid");
+    } else {
+        const descuentoC = usuarioCupon.descuento;
+        const precioFinal = calcularPrecioconDescuento(inputPrecio, descuentoC)
+        const resultado = document.getElementById("ResultadoCupon");
+        resultado.innerText = "The final price with the coupon discount is: $" + precioFinal;
+    }    
 }
